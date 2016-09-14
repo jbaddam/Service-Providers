3a6ff36873bf943deb8aac55940618a0c2136d23
lohith55 <lthota.bcj@gmail.com>
2016-09-14 11:25:08 -0500

    initial commit

---
+++ DiscoverBank/src/main/java/com/bofa/ccservices/verifycard/controller/CreditCardController.java
@@ -0,0 +1,119 @@
+package com.bofa.ccservices.verifycard.controller;
+
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bofa.ccservices.verifycard.model.CreditCard;
+import com.bofa.ccservices.verifycard.webservice.BofaCCWebService;
+
+/**
+ * Servlet implementation class CitiBankRegistration
+ */
+@Controller
+public class CreditCardController {
+	
+	 @Autowired
+	 BofaCCWebService ccServices;
+	 
+	 boolean result;
+
+	/* Accept credit card info in the form of a JSON string and give the response in boolean format. */
+	@RequestMapping(value = "/BankofAmerica", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON)
+	
+	public @ResponseBody boolean verifyCreditCardDetails(@RequestBody String cerditCardfromUser) {
+		
+		try {
+			result = ccServices.verifyCreditCardDetails(cerditCardfromUser);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * private static final long serialVersionUID = 1L; String reference;
+	 * 
+	 *//**
+		 * @see HttpServlet#doGet(HttpServletRequest request,
+		 *      HttpServletResponse response)
+		 */
+	/*
+	 * protected void doGet(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException {
+	 * 
+	 * CreditCard creditCardUserInput = new CreditCard(); BillingAddress
+	 * billingAddressUserInput = new BillingAddress();
+	 * 
+	 * 
+	 * we are getting parameters from HttpServletRequest and we are setting them
+	 * to the Customer class object c using setters
+	 * 
+	 * creditCardUserInput.setCreditCard(request.getParameter("txtCardNumber"));
+	 * creditCardUserInput.setNameOnCard(request.getParameter("txtNameOnCard"));
+	 * creditCardUserInput.setCvv(request.getParameter("txtcvv"));
+	 * creditCardUserInput.setExpirationDate(request.getParameter("expdate"));
+	 * 
+	 * billingAddressUserInput.setLine1(request.getParameter("txtline1"));
+	 * billingAddressUserInput.setLine2(request.getParameter("txtline2"));
+	 * billingAddressUserInput.setCity(request.getParameter("txtcity"));
+	 * billingAddressUserInput.setState(request.getParameter("txtstate"));
+	 * billingAddressUserInput.setZip(request.getParameter("txtzip"));
+	 * 
+	 * creditCardUserInput.setBillingAddress(billingAddressUserInput);
+	 * 
+	 * System.out.println(creditCardUserInput.toString());
+	 * System.out.println(billingAddressUserInput.toString());
+	 * 
+	 * 
+	 * Assigning the return value from getInfo method of
+	 * ActivationProcessCitibank class
+	 * 
+	 * boolean responseFromDatabase =
+	 * BofaCCWebService.verifyCreditCardDetails(creditCardUserInput);
+	 * 
+	 * PrintWriter out = response.getWriter();
+	 * 
+	 * 
+	 * These statements print the response from the DAO layer
+	 * 
+	 * try {
+	 * 
+	 * if (responseFromDatabase == true) {
+	 * 
+	 * out.println("Transaction Processed..!!");
+	 * 
+	 * } else {
+	 * 
+	 * out.println("Wrong details entered..!! Transaction can't be processed");
+	 * } } catch (Exception NullPointerException) {
+	 * System.out.println("Card Activation Unsuccessful");
+	 * 
+	 * }
+	 * 
+	 * }
+	 * 
+	 *//**
+		 * @see HttpServlet#doPost(HttpServletRequest request,
+		 *      HttpServletResponse response)
+		 *//*
+		 * protected void doPost(HttpServletRequest request, HttpServletResponse
+		 * response) throws ServletException, IOException {
+		 * 
+		 * doGet(request, response); c.setCreditCard(getServletInfo()); }
+		 */
+
+}
